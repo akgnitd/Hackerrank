925db62bc9ea871b7084925ec6ab79ceda8e01ca
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2020-02-29 13:29:17 +0530

    Update JavaDatatypes.java

--- Java/JavaDatatypes.java
+++ Java/JavaDatatypes.java
@@ -1,28 +1,22 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Solution{
-    public static void main(String []argh)
-    {
+public class JavaDatatypes {
+    public static void main(String[] argh) {
         Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
+        int integerInput = sc.nextInt();
 
-        for(int i=0;i<t;i++)
-        {
-            try
-            {
-                long x=sc.nextLong();
-                System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
-                if(x>=-32768 && x<=32767)System.out.println("* short");
-                if(x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1)
-                System.out.println("* int");
-                if(x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1)
-                System.out.println("* long");
-            }
-            catch(Exception e)
-            {
-                System.out.println(sc.next()+" can't be fitted anywhere.");
+        for (int i = 0; i < integerInput; i++) {
+            try {
+                long longInput = sc.nextLong();
+                System.out.println(longInput + " can be fitted in:");
+                if (longInput >= -128 && longInput <= 127) System.out.println("* byte");
+                if (longInput >= -32768 && longInput <= 32767) System.out.println("* short");
+                if (longInput >= -Math.pow(2, 31) && longInput <= Math.pow(2, 31) - 1)
+                    System.out.println("* int");
+                if (longInput >= -Math.pow(2, 63) && longInput <= Math.pow(2, 63) - 1)
+                    System.out.println("* long");
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
             }
         }
     }
