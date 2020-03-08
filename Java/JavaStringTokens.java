e412bcc3888382f2300bfedf47e1da98c51c93e6
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2020-03-08 21:22:22 +0530

    Create JavaStringTokens.java

---
+++ Java/JavaStringTokens.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+        if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+        String[] words = s.split("[^a-zA-Z]+");
+        System.out.println(words.length);
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                System.out.println(word);
+            }
+        }
+        scan.close();
+    }
+}
+
