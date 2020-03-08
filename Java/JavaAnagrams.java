fd311a803f87f3ef00cc306930b53cfc66b3dc9f
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2020-03-08 19:53:35 +0530

    Update JavaAnagrams.java

--- Java/JavaAnagrams.java
+++ Java/JavaAnagrams.java
@@ -12,21 +12,15 @@ public class JavaAnagrams {
         System.out.println((ret) ? "Anagrams" : "Not Anagrams");
     }
 
-    static boolean isAnagram(String a, String b) {
+static boolean isAnagram(String a, String b) {
         // Complete the function
         if (a.length() != b.length()) return false;
-        a = a.toLowerCase();b = b.toLowerCase();
+        a = a.toLowerCase();
+        b = b.toLowerCase();
         int asciiA = 0, asciiB = 0;
-        //ASCII Verification
         for (int i = 0; i < a.length(); i++) {
             asciiA = asciiA + (int) a.toCharArray()[i];
             asciiB = asciiB + (int) b.toCharArray()[i];
-        }
-        if (asciiA != asciiB) {
-            return false;
-        }
-        //Characters presence verification
-        for (int i = 0; i < a.length(); i++) {
             char aa = a.charAt(i);
             char bb = b.charAt(i);
             if (b.contains(Character.toString(aa)) && a.contains(Character.toString(bb))) {
@@ -34,6 +28,9 @@ public class JavaAnagrams {
             } else {
                 return false;
             }
+        }
+        if (asciiA != asciiB) {
+            return false;
         }
         return true;
     }
