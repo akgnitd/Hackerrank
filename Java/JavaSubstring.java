7025efe407784c9d958e1efd332e44a4c7fb3909
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2020-03-10 19:27:41 +0530

    Create JavaSubstring.java

---
+++ Java/JavaSubstring.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        System.out.println(s.substring(start, end));
+    }
+}
